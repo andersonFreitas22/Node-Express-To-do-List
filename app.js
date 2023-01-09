cb5cf2599a2c95dc05208e2f98b91d5b8f701262
Anderson Costa <f.anderson@aluno.ufabc.edu.br>
2023-01-08 22:38:22 -0300

    adicionando um arquivo js para criar as checklists

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const checkListRoute = require("./src/routes/checklist");
+
+app.use("/checklists", checkListRoute);
+
+app.listen(3000, () => {
+  console.log("Servidor ativo!");
+});
